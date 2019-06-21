d082595847a26aa7f7073b60a5f5b34b5e31c5af
Franco Maieli <maieli.franco@gmail.com>
2019-06-21 02:00:49 -0300

    Created Solution.cs for A Very Big Sum

---
+++ Algorithms/Warmup/A Very Big Sum/C#/Solution.cs
@@ -0,0 +1,43 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the aVeryBigSum function below.
+    static long aVeryBigSum(long[] ar) {
+        int minValue = 1;
+        int maxValue = 10;
+        long totalAmount = 0;
+        if (ar.Length >= minValue && ar.Length <= maxValue)
+        {
+            totalAmount = ar.Sum();
+        }
+        return totalAmount;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int arCount = Convert.ToInt32(Console.ReadLine());
+
+        long[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt64(arTemp))
+        ;
+        long result = aVeryBigSum(ar);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
